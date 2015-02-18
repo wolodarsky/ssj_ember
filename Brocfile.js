9f2835fe55a7177af9b2e6490ca37f9017f429a8
wolodarsky <tysonsafavi@me.com>
2015-02-17 22:32:50 -0800

    dependency updates

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,9 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
+  vendorFiles: {
+    'handlebars.js': null
+  },
   fingerprint: {
     prepend: 'https://s3.amazonaws.com/ssj-bucket/'
   }
