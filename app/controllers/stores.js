5a4603b15f146a64007a058131148e9e2fa0079d
wolodarsky <tysonsafavi@me.com>
2015-03-11 23:53:50 -0700

    grouping stores

---
+++ app/controllers/stores.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  groupedStores: function() {
+    let ojoj = this.get('model');
+    return $.map(ojoj.reduce((arr, item) => {
+      let state = item.get('state');
+      arr[state] = arr[state] || []
+      arr[state].push(item);
+      return arr;
+    }, {}), (stores, state) => {
+      console.log(state);
+      return {
+        state: state,
+        stores: stores
+      }
+    });
+  }.property('model.@each.state')
+});
