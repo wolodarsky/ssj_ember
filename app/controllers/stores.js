b13cc706bbbc7ba14b80d672b44f4da101bc5f52
wolodarsky <tysonsafavi@me.com>
2015-04-01 23:43:34 -0700

    layout updates

--- app/controllers/stores.js
+++ app/controllers/stores.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  /**
+   * Reduce the shops to a group states
+   */
   groupedStores: Ember.computed('model.@each.state', function() {
-    let ojoj = this.get('model');
-    return Ember.$.map(ojoj.reduce((arr, item) => {
+    let states = this.get('model').sortBy('state')
+    return Ember.$.map(states.reduce((arr, item) => {
       let state = item.get('state');
       arr[state] = arr[state] || [];
       arr[state].push(item);
       return arr;
     }, {}), (stores, state) => {
-      console.log(state);
       return {
         state: state,
         stores: stores
