4b2a5899e05235864ce043634344c95d68ebf322
wolodarsky <tysonsafavi@me.com>
2015-04-07 21:05:51 -0700

    formatting cleanup

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-    authenticate: function() {
-      let data = this.getProperties('identification', 'password');
-      return this.get('session').authenticate('simple-auth-authenticator:devise', data);
-    }
+      authenticate: function() {
+        let data = this.getProperties('identification', 'password');
+        return this.get('session').authenticate('simple-auth-authenticator:devise', data);
+      }
   }
 });
