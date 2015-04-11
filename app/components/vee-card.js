b9e5d7b4c8de82fe4086bedb243d73e6b5f0d06d
wolodarsky <tysonsafavi@me.com>
2015-04-10 17:19:43 -0700

    setup ver card to handle edits

--- app/components/vee-card.js
+++ app/components/vee-card.js
@@ -2,5 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'span',
-  classNames: ['vee-card']
+  classNames: ['vee-card'],
+
+  actions: {
+
+    save: function(model) {
+      model.save().then( () => {
+        this.transitionTo('shops');
+      }, () =>  {
+        this.set('error', 'Unable to Save Change');
+      });
+    },
+
+    cancel: (model) => {
+      model.rollback();
+      this.transitionTo('shops');
+    },
+
+    destroy: function(model) {
+      model.destroyRecord();
+      this.transitionTo('shops');
+    },
+
+    editShop: function() {
+      this.set('isEditing', true);
+    }
+
+  }
+
 });
