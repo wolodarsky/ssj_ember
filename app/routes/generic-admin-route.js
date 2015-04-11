f0599532e3dc7a6f3494e192f3d8121a4036a9ea
wolodarsky <tysonsafavi@me.com>
2015-04-11 15:25:52 -0700

    updated generic admin route

--- app/routes/generic-admin-route.js
+++ app/routes/generic-admin-route.js
@@ -5,14 +5,12 @@ import Configuration from 'simple-auth/configuration';
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   beforeModel: function(transition) {
     var superResult = this._super(transition);
+    var sessionPropertyName = Configuration.sessionPropertyName;
 
-    console.log('testing');
-    if (!this.get(Configuration.sessionPropertyName).get('isAdmin')) {
+    if (this.get(sessionPropertyName).get('isAuthenticated') &&
+        !this.get(sessionPropertyName).get('isAdmin')) {
       transition.abort();
-      this.get(Configuration.sessionPropertyName).set('attemptedTransition', transition);
-      Ember.assert('The route configured as Configuration.authenticationRoute cannot implement the AuthenticatedRouteMixin mixin as that leads to an infinite transitioning loop!', this.get('routeName') !== Configuration.authenticationRoute);
-      transition.send('sessionRequiresAuthentication');
-      console.log('testing');
+      this.transitionTo(Configuration.routeAfterAuthentication);
     }
 
     return superResult;
