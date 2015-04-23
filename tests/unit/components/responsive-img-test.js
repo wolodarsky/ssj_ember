137048d6d4e402fd38b95ff2b6b06befec86e9ac
wolodarsky <tysonsafavi@me.com>
2015-04-22 17:29:29 -0700

    using helper so images get fingerprinted

--- tests/unit/components/responsive-img-test.js
+++ tests/unit/components/responsive-img-test.js
@@ -5,14 +5,14 @@ import {
 
 moduleForComponent('responsive-img', {
   // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['helper:string-replacer']
 });
 
 test('it renders', function(assert) {
   assert.expect(2);
 
   // Creates the component instance
-  var component = this.subject();
+  var component = this.subject({src: 'some-text'});
   assert.equal(component._state, 'preRender');
 
   // Renders the component to the page
